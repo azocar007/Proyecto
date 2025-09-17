b3f9233a54a8c49b59b00f9084f25bdc51b79efb
azocar007 <166507820+azocar007@users.noreply.github.com>
2025-09-16 21:16:35 -0500

    Add files via upload

--- bot_azc/strategys/SMA_MACD_BB_GPT.py
+++ bot_azc/strategys/SMA_MACD_BB_GPT.py
@@ -13,9 +13,9 @@ class SMA_MACD_BB:
 
     def __init__(self,
                 df: pd.DataFrame,
-                last_price: float = None,
-                avg_price: float = None ,
                 decimales: int = None,
+                pip_price: int = None,
+                pip_mon: int = None,
                 indicator: str = None,
                 positionside: str = None,
                 monto_sl: float = None,
@@ -23,9 +23,9 @@ class SMA_MACD_BB:
                 ):
         self.df = df.copy()             # Copia del DataFrame para evitar modificaciones externas
         self.df1 = df.copy()            # Copia adicional para uso en preparación de DataFrame
-        self.last_price = last_price
-        self.avg_price = avg_price
         self.decimales = decimales
+        self.pip_price = pip_price
+        self.pip_mon = pip_mon
         self.indicator = indicator
         self.positionside = positionside
         self.monto_sl = monto_sl
@@ -33,8 +33,9 @@ class SMA_MACD_BB:
         self.dist_min = 0.25            # % 0 - 1 Distancia mínima entre el precio de entrada y el stop loss
         self.sep_sl = 25                # % de 0 - 100 ampliación de dist entre min_price y precio de entrada
         self.klines = 2
-        self._estado = {"websocket": True}
-        self._ventana = 1
+        self._ventana = 0
+        self._estado = {"cond_ventana": True, "websocket": True}
+        # Parámetros de la estrategia
         self.params = {
                         # Serie de datos
                         "serie_sma": "Close",        # avg_price - Close
@@ -53,8 +54,8 @@ class SMA_MACD_BB:
                         "macd_signal": 10,
                         }
 
+    # Calcula los indicadores SMA, MACD y Bandas de Bollinger
     def _calcular_indicadores(self):
-        """ Calcula los indicadores SMA, MACD y Bandas de Bollinger """
 
         # Serie datos para seleccionar del DataFrame sma - macd
         if self.params["serie_sma"] == "Close":
@@ -114,6 +115,7 @@ class SMA_MACD_BB:
         # Impresión del Dataframe para ensayo
         #print(f"DataFrame sin indicadores:\n{self.df1.tail(5).to_string(index=True)}")
         # Mostrar el DataFrame con indicadores al instanciar la clase
+        #"""
         print(f"\n{self.indicator} ESTRATEGIA - DataFrame dinamico con indicadores calculados de las ultimas {self.klines} velas cerradas:")
         print(self.df[[
                 "Time",
@@ -127,14 +129,15 @@ class SMA_MACD_BB:
                 "macd_line",
                 "macd_signal"
                 ]].tail(self.klines).to_string(index=True))
+        #"""
 
+    # Evaluación de condiciones sin WebSocket
     def condiciones_sin_websocket(self):
-        """ Evaluación de condiciones sin WebSocket """
 
-        if self._ventana >= self.params['ventana_max']:
+        if self._ventana > self.params['ventana_max']:
             print("⏳ Ventana de oportunidad caducada")
             self.reiniciar_condiciones()
-            return {"estrategia_valida": False}
+            return {"estrategia_valida": "reiniciar"}
 
         i = len(self.df) - 1
 
@@ -158,45 +161,59 @@ class SMA_MACD_BB:
                 self._estado['cond_2'] = True
                 print(f"✅✅ Condición 2: Cruce MACD bajista detectado {self.indicator} - {self.positionside}")
 
+    # Verifica si se requiere WebSocket para la estrategia
     def activar_websocket(self):
-        """ Verifica si se requiere WebSocket para la estrategia """
-        return self._estado.get('cond_1', False) and self._estado.get('cond_2', False) and self._estado.get('websocket', False)
+        if self._estado.get('cond_ventana', False) and self._estado.get('cond_1', False) and self._estado.get('cond_2', False) and self._estado.get('websocket', False):
+            return True
+        else:
+            return False
 
-    def evaluar_entrada(self, df_temp: pd.DataFrame):
+    # Evaluación de condiciones con WebSocket
+    def evaluar_entrada(self, df_temp: pd.DataFrame, last_price: float = None, avg_price: float = None):
 
-        if self._ventana >= self.params['ventana_max']:
+        if self._ventana > self.params['ventana_max']:
             print("⏳ Ventana de oportunidad caducada")
             self.reiniciar_condiciones()
-            return {"estrategia_valida": False}
+            return {"estrategia_valida": "reiniciar"}
 
         print(f"\n{self.indicator} ESTRATEGIA - DataFrame preparado para evaluación de entrada:\n")
-        #print(df_temp.tail(self.klines).to_string(index=True))
         print(df_temp.tail(self.klines))
 
         # Selección entre last_price y avg_price
         if self.params["serie_bb"] == "Avg_price":
-            df_temp.at[len(df_temp) - 1, 'Avg_price'] = self.avg_price
+            df_temp.at[len(df_temp) - 1, 'Avg_price'] = avg_price
             close_temp = df_temp['Avg_price']
-            price = self.avg_price
+            price = avg_price
 
         else: # self.params["serie_bb"] == "Close" or "ext_bb"
-            df_temp.at[len(df_temp) - 1, 'Close'] = self.last_price
+            df_temp.at[len(df_temp) - 1, 'Close'] = last_price
             close_temp = df_temp['Close']
-            price = self.last_price
+            price = last_price
 
         # Recalculo de Bandas de Bollinger con el último precio
         bb2_recalc = BollingerBands(close_temp, window=self.params["bb2_window"], window_dev=self.params["bb2_dev"])
-        bb_upper = bb2_recalc.bollinger_hband().iloc[-1]
-        bb_lower = bb2_recalc.bollinger_lband().iloc[-1]
+        bb_upper = round(bb2_recalc.bollinger_hband().iloc[-1], self.decimales)
+        bb_lower = round(bb2_recalc.bollinger_lband().iloc[-1], self.decimales)
+
+        # Recalculo de Bandas de Bollinger con el último precio para SL
+        bb1_recalc = BollingerBands(close_temp, window=self.params["bb1_window"], window_dev=self.params["bb1_dev"])
+        bb1_upper = round(bb1_recalc.bollinger_hband().iloc[-2], self.decimales)
+        bb1_lower = round(bb1_recalc.bollinger_lband().iloc[-2], self.decimales)
+        #print(f"{self.indicator} Precio de calculo SL: {price}, BB1_Upper: {bb1_upper} {type(bb1_upper)}, BB1_Lower: {bb1_lower}")
 
         if self.positionside == "LONG":
             # Condición de entrada para LONG
-            if price > bb_upper:
-                min_price = self.df['Low'].iloc[-self.params["bb2_window"]:].min()
-                dist_valid = mgo.dist_valida_sl(price, min_price, self.dist_min, self.sep_sl, self.monto_sl, self.ratio, 'long')
-                stop_loss = dist_valid.get("stop_loss", None)
-                cant_mon = dist_valid.get("cant_mon", None)
-                if stop_loss:
+            if price >= bb_upper:
+                # Cálculo del precio mínimo en la ventana de las últimas 'bb2_window' velas
+                #min_price = self.df['Low'].iloc[-self.params["bb2_window"]:].min()
+                # Calculo del precio mínimo empleando la banda inferior de bollinger
+                min_price = mgo.redondeo(bb1_lower, self.pip_price)
+                price = mgo.redondeo(price, self.pip_price)
+                dist_valid = mgo.dist_valida_sl(price, min_price, self.dist_min, self.sep_sl, self.monto_sl, self.ratio, self.positionside)
+                stop_loss = mgo.redondeo(dist_valid.get("stop_loss", None), self.pip_price)
+                take_profit = mgo.redondeo(dist_valid.get("take_profit", None), self.pip_price)
+                cant_mon = mgo.redondeo(dist_valid.get("cant_mon", None), self.pip_mon)
+                if dist_valid.get("distancia", False):
                     print("✅✅✅ Condición 3 cumplida: Ruptura y SL válido")
                     #return {"estrategia_valida": False}
                     #"""
@@ -205,6 +222,7 @@ class SMA_MACD_BB:
                             "stop_loss": stop_loss,
                             "ref_price":min_price,
                             "cant_mon": cant_mon,
+                            "take_profit": take_profit,
                             "estrategia_valida": True
                             }
                     #"""
@@ -212,12 +230,17 @@ class SMA_MACD_BB:
 
         elif self.positionside == "SHORT":
             # Condición de entrada para SHORT
-            if price < bb_lower:
-                max_price = self.df['High'].iloc[-self.params["bb2_window"]:].max()
-                dist_valid = mgo.dist_valida_sl(price, max_price, self.dist_min, self.sep_sl, self.monto_sl, self.ratio, 'short')
-                stop_loss = dist_valid.get("stop_loss", None)
-                cant_mon = dist_valid.get("cant_mon", None)
-                if stop_loss:
+            if price <= bb_lower:
+                # Cálculo del precio máximo en la ventana de las últimas 'bb2_window' velas
+                #max_price = self.df['High'].iloc[-self.params["bb2_window"]:].max()
+                # Calculo del precio máximo empleando la banda superior de bollinger
+                max_price = mgo.redondeo(bb1_upper, self.pip_price)
+                price = mgo.redondeo(price, self.pip_price)
+                dist_valid = mgo.dist_valida_sl(price, max_price, self.dist_min, self.sep_sl, self.monto_sl, self.ratio, self.positionside)
+                stop_loss = mgo.redondeo(dist_valid.get("stop_loss", None), self.pip_price)
+                take_profit = mgo.redondeo(dist_valid.get("take_profit", None), self.pip_price)
+                cant_mon = mgo.redondeo(dist_valid.get("cant_mon", None), self.pip_mon)
+                if dist_valid.get("distancia", False):
                     print("✅✅✅ Condición 3 cumplida: Ruptura y SL válido")
                     #return {"estrategia_valida": False}
                     #"""
@@ -226,21 +249,27 @@ class SMA_MACD_BB:
                             "stop_loss": stop_loss,
                             "ref_price":max_price,
                             "cant_mon": cant_mon,
+                            "take_profit": take_profit,
                             "estrategia_valida": True
                             }
                     #"""
             return {"estrategia_valida": False}
 
+    # Reinicia las condiciones del estado
     def reiniciar_condiciones(self):
-        # Reinicia las condiciones del estado
-        self._estado = {"websocket": True}
-        self._ventana = 1
+        self._estado = {"cond_ventana": True, "websocket": True}
+        self._ventana = 0
         print(f"🔄 Condiciones reiniciadas {self.indicator} - {self.positionside}")
 
+    # Incrementa la ventana de oportunidad
     def incrementar_ventana(self):
-        # Incrementa la ventana de oportunidad
         self._ventana += 1
+        if self._ventana > self.params['ventana_max']:
+            self._estado['cond_ventana'] = False
+        else:
+            self._estado['cond_ventana'] = True
         print(f"🔼 Ventana incrementada a {self._ventana}, {self.indicator} - {self.positionside}")
 
+    # Verifica si la estrategia requiere WebSocket
     def requiere_websocket(self):
         return self._estado.get('websocket', False)
